e8a11235ed2b122104b276a1b6cb95e538e1c126
wonyejin05100 <151604001+wonyejin05100@users.noreply.github.com>
2023-12-29 17:55:50 +0900

    Add files via upload

---
+++ js/live.js
@@ -0,0 +1,17 @@
+// toggle 
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+// close
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
+// popup
+$(function(){
+    $(".btn").on("click",function(){
+        $("#popup").fadeIn();
+    });
+    $(".close").on("click",function(){
+        $("#popup").fadeOut();
+    });
+});
